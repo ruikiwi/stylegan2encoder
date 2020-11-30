39ab44603a8e83313a7c474e9da8d4917e073c09
ruikiwi <ruikiwi@users.noreply.github.com>
2020-11-30 13:13:00 -0500

    Update projector.py

--- projector.py
+++ projector.py
@@ -15,7 +15,8 @@ from training import misc
 
 class Projector:
     def __init__(self,
-        vgg16_pkl                       = 'https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2',
+        #vgg16_pkl                       = 'https://drive.google.com/uc?id=1N2-m9qszOeVC9Tq77WxsLnuWwOedQiD2',
+        vgg16_pkl                       = 'networks/vgg16_zhang_perceptual.pkl',
         num_steps                       = 1000,
         initial_learning_rate           = 0.1,
         initial_noise_factor            = 0.05,
