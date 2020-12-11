f11ee7dca6cc7a9740d3dc8fb555e44597d2efc1
ruikiwi <ruikiwi@users.noreply.github.com>
2020-12-10 20:04:18 -0500

    Update run_projector.py

--- run_projector.py
+++ run_projector.py
@@ -16,6 +16,9 @@ import pretrained_networks
 from training import dataset
 from training import misc
 
+# --- Custom Change ---
+import pickle
+
 #----------------------------------------------------------------------------
 
 def project_image(proj, targets, png_prefix, num_snapshots):
@@ -28,7 +31,12 @@ def project_image(proj, targets, png_prefix, num_snapshots):
         if proj.get_cur_step() in snapshot_steps:
             misc.save_image_grid(proj.get_images(), png_prefix + 'step%04d.png' % proj.get_cur_step(), drange=[-1,1])
     print('\r%-30s\r' % '', end='', flush=True)
-
+   
+    # --- Custom Change ---
+    with open(png_prefix + 'final_latent_code.pkl', 'wb') as out_file:
+        print("shape of dlatent", proj.get_dlatents().shape)
+        print("saving dlatent after iteration")
+        pickle.dump(proj.get_dlatents(), out_file)
 #----------------------------------------------------------------------------
 
 def project_generated_images(network_pkl, seeds, num_snapshots, truncation_psi):
